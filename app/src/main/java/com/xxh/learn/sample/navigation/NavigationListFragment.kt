d0fc17ae0786ed06c74775a7a98072d62da50596
xxh <fenglin8866@163.com>
2024-10-31 14:28:29 +0800

    Navigation之NavigationAdvancedSample

--- app/src/main/java/com/xxh/learn/sample/navigation/NavigationListFragment.kt
+++ app/src/main/java/com/xxh/learn/sample/navigation/NavigationListFragment.kt
@@ -2,6 +2,7 @@ package com.xxh.learn.sample.navigation
 
 import android.content.Intent
 import com.android.samples.donuttracker.DonutMainActivity
+import com.example.android.navigationadvancedsample.NavigationSampleActivity
 import com.xxh.basic.ListBaseFragment
 import com.xxh.learn.fragment.NavigationFragmentActivity
 
@@ -10,12 +11,14 @@ class NavigationListFragment : ListBaseFragment(){
     override fun setData(): Array<String> = arrayOf(
         "NavigationFragment",
         "DonutTracker",
+        "NavigationAdvancedSample",
     )
 
     override fun itemClickHandle(name: String) {
         val clazz: Class<*>? = when (name) {
             "NavigationFragment" -> NavigationFragmentActivity::class.java
             "DonutTracker" -> DonutMainActivity::class.java
+            "NavigationAdvancedSample" -> NavigationSampleActivity::class.java
             else -> null
         }
         clazz?.let {
